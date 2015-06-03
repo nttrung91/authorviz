21cd9c31ab52f98c71e6d77f072cb954e83ff092
Evelyn Luu <eyluu@uci.edu>
2015-06-02 17:37:21 -0700

    Revert "Changed anonymous author name to Anonymous"
    This reverts commit af76dc272602bd5897560583d3eea9c8acd6506c.

--- js/view.js
+++ js/view.js
@@ -322,20 +322,12 @@
 
     renderAuthorName: function() {
       var html = _.reduce(this.authors, function(memo, author, index, list) {
-          
-        // Change name if author is Anonymous
-        if(typeof author.name === 'undefined')
-        {
-            author.name = 'Anonymous';
-        }
-            
         if(index === list.length - 1) {
           return memo + '<span style="color:' + author.color + '">' + author.name + '</span>'
         }
 
         return memo + '<span style="color:' + author.color + '">' + author.name + ', </span>'
-      }
-                          ,'');
+      },'');
 
       return html;
     },
